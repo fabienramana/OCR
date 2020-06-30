c11a0c92a623b21ed99474683cb9fec23b8b92a7
fabien <fabien.rmnd@gmail.com>
2020-06-30 15:14:02 +0200

    add file to read

--- src/main/java/com/cleancode/OCR/Main.java
+++ src/main/java/com/cleancode/OCR/Main.java
@@ -1,7 +1,27 @@
 package com.cleancode.OCR;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String pathname = "src/main/java/com/cleancode/OCR/input.txt";
+        try {
+            File myObj = new File(pathname);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+            System.out.println("size :" + myObj.length());
+            
+        } catch (Exception e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
     }
 }
